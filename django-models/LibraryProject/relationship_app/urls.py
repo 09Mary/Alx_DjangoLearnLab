1a9333c8895b797982467c3e120f3de4ec6a3e39
May <marynyaboke999@gmail.com>
2025-09-07 11:37:53 +0300

    urls update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .views import register, CustomLoginView, CustomLogoutView
+from .views import list_books
 
 urlpatterns = [
     # Existing routes for your app
