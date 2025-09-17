9ffaca68c73672eed4fc869e0b475761ac8b6dd1
May <marynyaboke999@gmail.com>
2025-09-17 15:25:47 +0300

    AssignPermission

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    # logic to create book
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # logic to edit book
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # logic to delete book
+    return redirect("list_books")
