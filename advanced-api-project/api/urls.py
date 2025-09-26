0cd678dbfa74a15a52b828026865c200e755dffb
May <marynyaboke999@gmail.com>
2025-09-26 18:30:12 +0300

    FixUrl

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -6,5 +6,6 @@ from .views import (
 urlpatterns = [
     path('books/update/<int:id>/', views.update_book, name='update_book'),
     path('books/delete/<int:id>/', views.delete_book, name='delete_book'),
+    path('books/create/<int:id>/', views.delete_book, name='create_book'),
 
 ]
