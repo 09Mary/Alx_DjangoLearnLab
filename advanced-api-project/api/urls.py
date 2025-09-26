6215df46f2872b9775ebae425beb08d7f6f4f78a
May <marynyaboke999@gmail.com>
2025-09-26 18:24:54 +0300

    CustomGenericViews

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,10 +4,7 @@ from .views import (
     BookDeleteView, BookUpdateView
 )
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/<int:id>/', views.update_book, name='update_book'),
+    path('books/delete/<int:id>/', views.delete_book, name='delete_book'),
 
 ]
