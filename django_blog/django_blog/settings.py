b9e4414388f5501461255181260180f9f10d6afc
May <marynyaboke999@gmail.com>
2025-10-03 16:33:31 +0300

    Authetication

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -124,7 +124,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-    STATIC_URL = 'static/'
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [BASE_DIR / "static"]  # for project-level static files
+    STATIC_ROOT = BASE_DIR / "staticfiles"    # where collectstatic will gather them
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
